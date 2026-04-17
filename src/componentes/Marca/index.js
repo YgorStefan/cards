9eea5e1ba9411f95d56c393ef429d052d649ac44
Ygor Stefankowski <ygor.stefan@gmail.com>
2026-04-17 18:05:29 -0300

    atualiza Marca com lista de ProdutoItem e botão excluir

--- src/componentes/Marca/index.js
+++ src/componentes/Marca/index.js
@@ -1,16 +1,47 @@
-import Produto from "../Produto"
+import React from 'react'
+import ProdutoItem from '../ProdutoItem'
 import './Marca.css'
 
-const Marca = (props) => {
-    return (
-        (props.produtos.length > 0) ? <section className="marca" style={{backgroundColor: props.corPrimaria}} >
-            <h3 style={{borderColor: props.corSecundaria }}>{props.nome}</h3>
-            <div className='produtos'>
-            {props.produtos.map(produto => <Produto corDeFundo={props.corSecundaria} key={produto.nome} nome={produto.nome} codigo={produto.codigo} imagem={produto.imagem}/>)}
-            </div>
-        </section>
-        : ''
-    )
+const Marca = ({ marca, produtos, onExcluirMarca, onEditarProduto, onExcluirProduto }) => {
+  return (
+    <section className="marca" style={{ backgroundColor: marca.corPrimaria }}>
+      <div className="marca__cabecalho">
+        <div className="marca__titulo">
+          <div className="marca__dot" style={{ backgroundColor: marca.corSecundaria }} />
+          <h3 style={{ color: marca.corSecundaria }}>{marca.nome}</h3>
+          <span
+            className="marca__badge"
+            style={{
+              color: marca.corSecundaria,
+              background: `${marca.corSecundaria}22`
+            }}
+          >
+            {produtos.length} {produtos.length === 1 ? 'produto' : 'produtos'}
+          </span>
+        </div>
+        <button
+          className="marca__excluir"
+          type="button"
+          onClick={() => onExcluirMarca(marca.id)}
+        >
+          Excluir marca
+        </button>
+      </div>
+      {produtos.length > 0 && (
+        <div className="marca__lista">
+          {produtos.map(produto => (
+            <ProdutoItem
+              key={produto.id}
+              produto={produto}
+              corSecundaria={marca.corSecundaria}
+              onEditar={onEditarProduto}
+              onExcluir={onExcluirProduto}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default Marca
