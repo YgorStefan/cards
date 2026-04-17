4309705a4c2e44f86568d1a36ebb897f56bd3255
Ygor Stefankowski <ygor.stefan@gmail.com>
2026-04-17 18:05:29 -0300

    integra App.js com CRUD completo, localStorage e Drawer

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
-import {useState} from "react"
-import Banner from "./componentes/Banner"
-import Formulario from "./componentes/Formulario"
-import Marca from "./componentes/Marca"
+// src/App.js
+import React, { useState } from 'react'
+import useLocalStorage from './hooks/useLocalStorage'
+import Header from './componentes/Header'
+import Marca from './componentes/Marca'
+import Drawer from './componentes/Drawer'
+import EstadoVazio from './componentes/EstadoVazio'
+
+const marcasIniciais = [
+  { id: '1', nome: 'Jetmax', corPrimaria: '#D9F7E9', corSecundaria: '#57C278' },
+  { id: '2', nome: 'Kajima', corPrimaria: '#E8F8FF', corSecundaria: '#82CFFA' },
+  { id: '3', nome: 'KWS', corPrimaria: '#F0F8E2', corSecundaria: '#A6D157' },
+  { id: '4', nome: 'Kawashima', corPrimaria: '#FDE7E8', corSecundaria: '#E06B69' },
+  { id: '5', nome: 'Nakashi', corPrimaria: '#FFF5D9', corSecundaria: '#FFBA05' },
+  { id: '6', nome: 'Outros', corPrimaria: '#FFEEDF', corSecundaria: '#FF8A29' },
+]
 
 function App() {
+  const [produtos, setProdutos] = useLocalStorage('cdp_produtos', [])
+  const [marcas, setMarcas] = useLocalStorage('cdp_marcas', marcasIniciais)
+  const [drawer, setDrawer] = useState({ aberto: false, tipo: null, item: null })
 
-    const marcas = [
-        {
-            nome: 'Jetmax',
-            corPrimaria: '#D9F7E9',
-            corSecundaria: '#57C278'
-        },
-        {
-            nome: 'Kajima',
-            corPrimaria: '#E8F8FF',
-            corSecundaria: '#82CFFA'
-        },
-        {
-            nome: 'KWS',
-            corPrimaria: '#F0F8E2',
-            corSecundaria: '#A6D157'
-        },
-        {
-            nome: 'Kawashima',
-            corPrimaria: '#FDE7E8',
-            corSecundaria: '#E06B69'
-        },
-        {
-            nome: 'Nakashi',
-            corPrimaria: '#FFF5D9',
-            corSecundaria: '#FFBA05'
-        },
-        {
-            nome: 'Outros',
-            corPrimaria: '#FFEEDF',
-            corSecundaria: '#FF8A29'
-        },
-    ]
+  const abrirDrawer = (tipo, item = null) =>
+    setDrawer({ aberto: true, tipo, item })
 
-    const [produtos, setProdutos] = useState([])
+  const fecharDrawer = () =>
+    setDrawer({ aberto: false, tipo: null, item: null })
 
-    const aoNovoProdutoAdicionado = (produto) => {
-        console.log(produto)
-        setProdutos([...produtos, produto])
-    }
-
-    return (
-        <div className="App">
-            <Banner />
-            <Formulario marcas={marcas.map(marca => marca.nome)} aoProdutoCadastrado={produto => aoNovoProdutoAdicionado(produto)} />
-
-            {marcas.map(marca => <Marca
-                key={marca.nome}
-                nome={marca.nome}
-                corPrimaria={marca.corPrimaria}
-                corSecundaria={marca.corSecundaria}
-                produtos={produtos.filter(produto => produto.marca === marca.nome)}
-            />)}
-
-        </div>
+  const salvarProduto = (produto) => {
+    setProdutos(prev =>
+      prev.some(p => p.id === produto.id)
+        ? prev.map(p => p.id === produto.id ? produto : p)
+        : [...prev, produto]
     )
+    fecharDrawer()
+  }
+
+  const excluirProduto = (id) => {
+    setProdutos(prev => prev.filter(p => p.id !== id))
+  }
+
+  const salvarMarca = (marca) => {
+    setMarcas(prev => [...prev, marca])
+    fecharDrawer()
+  }
+
+  const excluirMarca = (id) => {
+    const marca = marcas.find(m => m.id === id)
+    setMarcas(prev => prev.filter(m => m.id !== id))
+    if (marca) {
+      setProdutos(prev => prev.filter(p => p.marca !== marca.nome))
+    }
+  }
+
+  return (
+    <div className="App">
+      <Header
+        onAbrirDrawerProduto={() => abrirDrawer('produto')}
+        onAbrirDrawerMarca={() => abrirDrawer('marca')}
+      />
+      {produtos.length === 0 && (
+        <EstadoVazio onAdicionar={() => abrirDrawer('produto')} />
+      )}
+      {marcas.map(marca => (
+        <Marca
+          key={marca.id}
+          marca={marca}
+          produtos={produtos.filter(p => p.marca === marca.nome)}
+          onExcluirMarca={excluirMarca}
+          onEditarProduto={(produto) => abrirDrawer('produto', produto)}
+          onExcluirProduto={excluirProduto}
+        />
+      ))}
+      <Drawer
+        aberto={drawer.aberto}
+        tipo={drawer.tipo}
+        item={drawer.item}
+        marcas={marcas}
+        onFechar={fecharDrawer}
+        onSalvarProduto={salvarProduto}
+        onSalvarMarca={salvarMarca}
+      />
+    </div>
+  )
 }
 
 export default App
