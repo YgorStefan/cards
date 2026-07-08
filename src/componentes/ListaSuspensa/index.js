09942865af9e533e8c162ddf8f5c7c7bf9ae384e
Ygor Stefankowski <ygor.stefan@gmail.com>
2026-07-08 01:41:10 -0300

    Declara devDependencies de teste e higieniza gitignore

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    // console.log(props.itens)
     const id = `lista-${props.label?.toLowerCase().replace(/\s+/g, '-')}`
     return (
         <div className="lista-suspensa">
